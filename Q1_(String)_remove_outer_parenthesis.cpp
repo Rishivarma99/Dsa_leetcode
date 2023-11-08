0239e5bda536b9395355439ea741c2c73f79d74e
Rishivarma99 <95839748+Rishivarma99@users.noreply.github.com>
2023-11-08 09:17:00 +0530

    Q1_(String)_remove_outer_parenthesis.cpp

--- Q1_(String)_remove_outer_parenthesis.cpp
+++ Q1_(String)_remove_outer_parenthesis.cpp
@@ -1,5 +1,5 @@
-// given a string that contains mutiple paranthesis now we shoud divide the string into any no of valid paranthesis 
-// and remove each of their outer paranthsis and prind the result 
+//Given a string that contains multiple parenthesis now we should divide the string into any number of valid parenthesis 
+// and remove each of their outer parenthesis and print the result 
 
 // Ex-1:(()())(())
 // SOL:  -> divide into valid paren.. -> (()()) + (()) -> remove their outer pare... -> ()()+() ->combine -> ()()() 
@@ -9,12 +9,22 @@
 // (USING STACK) (STACK-> st.push(),st.pop(),st.top(),st.size()) 
 // (String-> declare,s.push_back(ch),s.length()) (Char -> to store each string )
 
+// APP2 : TC : O(N)  SC: O(N) TO STORE ANS 
+//  USING COUNT METHOD 
+// 1.WE COUNT NO OF OPENING AND CLOSING PARENTHESIS USING FOR LOOP 
+// 2.IF WHENEVER OP AND CL PAREN ARE EQUAL THEN WE EXTRACT THE IN-BETWEEN OUTER PARENTHESIS
+// 3. USING SUBSTR FUNCTION START+1, I-(START+1)
+// 4.RETURN ANS 
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution 
+{
 public:
     string removeOuterParentheses(string s) {
+
+     // APP 1 : 
       stack <char> st;
       string ans="";
 
@@ -40,10 +50,31 @@ public:
               
      }
            return ans;
+
+     // APP 2: USING SUBSTR FUNCTION 
+
+            int op_paren=0;
+            int cl_paren=0;
+            //Here start always point to the outer opening parenthesis 
+            int start =0;
+            string ans="";
+
+            for(int i=0;i<s.length();i++){
+                if(s[i]=='(' ){
+                    op_paren++;
+                }
+                else if(s[i]==')'){
+                    cl_paren++;
+                }
+
+                if(op_paren==cl_paren){
+                    ans+=s.substr(start+1,i-(start+1));
+                    start=i+1;
+                }
+            }
+            return ans;
+     
            }
 
-
-         
-    
 };
 
